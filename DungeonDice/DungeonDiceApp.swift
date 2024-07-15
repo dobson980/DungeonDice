3a5192b75654699eccaf3922a5d4e48fe0fbe373
Tom Dobson <dobson980@gmail.com>
2024-07-14 20:11:28 -0700

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Thomas Dobson on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
